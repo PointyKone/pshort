f1746202d2caa969e77f3b5f217a0d84a7e208ad
Kian Saeidnia <ksaeidnia06@gmail.com>
2022-06-18 13:24:09 +1000

    perf: :zap: readded caching

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,16 +1,21 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest, ev: NextFetchEvent, next: () => void) {
-    if (req.nextUrl.pathname.startsWith("/api/get-url")) {
-        console.log("returning early")
-        return
-    }
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  if (
+    req.nextUrl.pathname.startsWith("/api/") ||
+    req.nextUrl.pathname === "/"
+  ) {
+    return;
+  }
+  const slug = req.nextUrl.pathname.split("/").pop();
 
-    const slug = req.nextUrl.pathname.split("/").pop();
+  const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
+  if (slugFetch.status === 404) {
+    return NextResponse.redirect(req.nextUrl.origin);
+  }
+  const data = await slugFetch.json();
 
-    const data = await (await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`)).json();    
-
-    if (data?.url) {
-        return NextResponse.redirect(data.url);
-    }
+  if (data?.url) {
+    return NextResponse.redirect(data.url);
+  }
 }
--- src/pages/api/get-url/[slug].ts
+++ src/pages/api/get-url/[slug].ts
@@ -1,33 +1,40 @@
 import { NextApiRequest, NextApiResponse } from "next";
+
 import { prisma } from "../../../db/client";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	const slug = req.query["slug"];
+  const slug = req.query["slug"];
 
-	if (!slug || typeof slug !== "string") {
-		res.status(404);
-		res.send(JSON.stringify({ message: "No slug provided." }));
-		return;
-	}
+  if (!slug || typeof slug !== "string") {
+    res.statusCode = 404;
 
-	const data = await prisma.shortLink.findFirst({
-		where: {
-			slug: {
-				equals: slug
-			}
-		}
-	})
+    res.send(JSON.stringify({ message: "pls use with a slug" }));
 
-	if (!data) {
-		res.status(404);
+    return;
+  }
 
-		// res.setHeader("Content-Type", "application/json");
-		// res.setHeader("Access-Control-Allow-Origin", "*");
-		// res.setHeader("Cache-Control", "s-maxage=1000000, stale-while-revalidate");
+  const data = await prisma.shortLink.findFirst({
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  });
 
-		res.send(JSON.stringify({ message: "Slug not found." }));
-		return;
-	}
+  if (!data) {
+    res.statusCode = 404;
 
-	return res.json(data);
-}
+    res.send(JSON.stringify({ message: "slug not found" }));
+
+    return;
+  }
+
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Cache-Control",
+    "s-maxage=1000000000, stale-while-revalidate"
+  );
+
+  return res.json(data);
+};
