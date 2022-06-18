815c9f2e215935e457a93d241456624462e4ddfd
Kian Saeidnia <ksaeidnia06@gmail.com>
2022-06-18 12:31:46 +1000

    feat: :sparkles: slugs with redirect added

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../db/client";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	const slug = req.query["slug"];
+
+	if (!slug || typeof slug !== "string") {
+		res.status(404);
+		res.send(JSON.stringify({ message: "No slug provided." }));
+		return;
+	}
+
+	const data = await prisma.shortLink.findFirst({
+		where: {
+			slug: {
+				equals: slug
+			}
+		}
+	})
+
+	if (!data) {
+		res.status(404);
+		res.send(JSON.stringify({ message: "Slug not found." }));
+		return;
+	}
+
+	return res.redirect(data.url)
+}
